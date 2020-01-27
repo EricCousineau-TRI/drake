75d7c21d1665e0af2bfcd1e392a5ea1b0beb7636
Eric Cousineau <eric.cousineau@tri.global>
2020-01-27 16:08:16 -0500

    issue happens when we use Ref<const MatrixXd>

--- tmp/convert_py.cc
+++ tmp/convert_py.cc
@@ -6,7 +6,7 @@ namespace py = pybind11;
 namespace {
 
 PYBIND11_MODULE(convert_py, m) {
-  m.def("pass_through", [](const Eigen::MatrixXd& A) {
+  m.def("pass_through", [](const Eigen::Ref<const Eigen::MatrixXd>& A) {
     return A;
   });
 }
