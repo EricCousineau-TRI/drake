da3b189c0277a52dfdd1a9c1f70a18fc08e71139
Eric Cousineau <eric.cousineau@tri.global>
2020-03-23 18:28:29 -0400

    cleanup

--- tmp/bleh.py
+++ tmp/bleh.py
@@ -2,16 +2,7 @@
 
 from pydrake.systems.primitives import ConstantVectorSource
 
-
-def main():
-    source = ConstantVectorSource([None])
-    context = source.CreateDefaultContext()
-    y = source.get_output_port(0).Eval(context)
-    print(y)
-
-
-# main()
-import sys, trace
-sys.stdout = sys.stderr
-tracer = trace.Trace(trace=1, count=0, ignoredirs=["/usr", sys.prefix])
-tracer.runfunc(main)
+source = ConstantVectorSource([None])
+context = source.CreateDefaultContext()
+y = source.get_output_port(0).Eval(context)
+print(y)
