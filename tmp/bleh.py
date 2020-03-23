fc3ee7a3b74ce824f7c158164f80db8b273865b4
Eric Cousineau <eric.cousineau@tri.global>
2020-03-23 19:14:03 -0400

    followup

--- tmp/bleh.py
+++ tmp/bleh.py
@@ -1,25 +1,14 @@
 # from https://drakedevelopers.slack.com/archives/C2CK4CWE7/p1584994164030500
 
 import numpy as np
-from pydrake.all import Simulator, Integrator, ConstantVectorSource, DiagramBuilder, SignalLogger
+from pydrake.all import (
+    DiagramBuilder, ConstantVectorSource, Integrator, Simulator)
 
-
-def main():
-    builder = DiagramBuilder()
-    u = np.nan
-    n = 1
-    source = builder.AddSystem(ConstantVectorSource([u]))
-    integrator = builder.AddSystem(Integrator(n))
-    logger = builder.AddSystem(SignalLogger(n))
-    builder.Connect(
-        source.get_output_port(0), integrator.get_input_port(0))
-    builder.Connect(
-        integrator.get_output_port(0), logger.get_input_port(0))
-    diagram = builder.Build()
-    simulator = Simulator(diagram)
-    simulator.AdvanceTo(0.)
-    print("Done")
-
-
-assert __name__ == "__main__"
-main()
+builder = DiagramBuilder()
+source = builder.AddSystem(ConstantVectorSource([np.nan]))
+integrator = builder.AddSystem(Integrator(1))
+builder.Connect(source.get_output_port(0), integrator.get_input_port(0))
+diagram = builder.Build()
+simulator = Simulator(diagram)
+simulator.AdvanceTo(0.)  # loops forever
+print("Done")
