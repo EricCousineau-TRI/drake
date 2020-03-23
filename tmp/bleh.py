662cfe5aaa326f2005628d9c29d74ac908ca4c83
Eric Cousineau <eric.cousineau@tri.global>
2020-03-23 18:23:03 -0400

    more minimal

--- tmp/bleh.py
+++ tmp/bleh.py
@@ -1,29 +1,13 @@
 # from https://drakedevelopers.slack.com/archives/C2CK4CWE7/p1584994164030500
 
-from pydrake.systems.analysis import Simulator
-from pydrake.systems.framework import DiagramBuilder, LeafSystem, BasicVector
-from pydrake.systems.primitives import ConstantVectorSource, Gain
-
-
-class Sink(LeafSystem):
-    def __init__(self):
-        LeafSystem.__init__(self)
-        self.input_port = self.DeclareVectorInputPort("u", BasicVector(1))
-
-    def DoPublish(self, context, events):
-        u = self.input_port.Eval(context)
-        print(f"Hey {u}")
+from pydrake.systems.primitives import ConstantVectorSource
 
 
 def main():
-    builder = DiagramBuilder()
-    source = builder.AddSystem(ConstantVectorSource([None]))
-    sink = builder.AddSystem(Sink())
-    builder.Connect(source.get_output_port(0), sink.input_port)
-
-    diagram = builder.Build()
-    context = diagram.CreateDefaultContext()
-    diagram.Publish(context)
+    source = ConstantVectorSource([None])
+    context = source.CreateDefaultContext()
+    y = source.get_output_port(0).Eval(context)
+    print(y)
 
 
 # main()
