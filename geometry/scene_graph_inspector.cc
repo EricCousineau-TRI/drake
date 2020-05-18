21e171ab91a696a3eacc0bf6f85baaa702d36cfa
Eric Cousineau <eric.cousineau@tri.global>
2020-05-18 19:15:04 -0400

    simplify

--- geometry/scene_graph_inspector.cc
+++ geometry/scene_graph_inspector.cc
@@ -3,61 +3,24 @@
 namespace drake {
 namespace geometry {
 
-namespace {
-
-template <typename T>
-class GeometryInstanceReifier : public ShapeReifier {
- public:
-  GeometryInstanceReifier(
-      const SceneGraphInspector<T>* inspector, GeometryId id)
-      : inspector_(inspector), id_(id) {}
-
-  using ShapeReifier::ImplementGeometry;
-  void ImplementGeometry(const Sphere& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const Cylinder& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const HalfSpace& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const Box& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const Capsule& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const Ellipsoid& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const Mesh& shape, void*) final { Run(shape); }
-  void ImplementGeometry(const Convex& shape, void*) final { Run(shape); }
-
-  std::unique_ptr<GeometryInstance> release_value() {
-    DRAKE_DEMAND(!!value_);
-    return std::move(value_);
-  }
-
- private:
-  template <typename ShapeType>
-  void Run(const ShapeType& shape) {
-    auto name = inspector_->GetName(id_);
-    auto X_PG = inspector_->GetPoseInFrame(id_);
-    value_ = std::make_unique<GeometryInstance>(
-        X_PG, std::make_unique<ShapeType>(shape), name);
-    if (auto* props = inspector_->GetProximityProperties(id_)) {
-      value_->set_proximity_properties(*props);
-    }
-    if (auto* props = inspector_->GetIllustrationProperties(id_)) {
-      value_->set_illustration_properties(*props);
-    }
-    if (auto* props = inspector_->GetPerceptionProperties(id_)) {
-      value_->set_perception_properties(*props);
-    }
-  }
-
-  const SceneGraphInspector<T>* inspector_{};
-  GeometryId id_;
-  std::unique_ptr<GeometryInstance> value_;
-};
-
-}  // namespace
-
 template <typename T>
 std::unique_ptr<GeometryInstance>
 SceneGraphInspector<T>::CloneGeometryInstance(GeometryId id) const {
-  GeometryInstanceReifier reifier(this, id);
-  Reify(id, &reifier);
-  return reifier.release_value();
+  const std::string name = GetName(id);
+  const math::RigidTransformd X_PG = GetPoseInFrame(id);
+  std::unique_ptr<Shape> shape = GetShape(id).Clone();
+  auto geometry_instance = std::make_unique<GeometryInstance>(
+      X_PG, std::move(shape), name);
+  if (const auto* props = GetProximityProperties(id)) {
+    geometry_instance->set_proximity_properties(*props);
+  }
+  if (const auto* props = GetIllustrationProperties(id)) {
+    geometry_instance->set_illustration_properties(*props);
+  }
+  if (const auto* props = GetPerceptionProperties(id)) {
+    geometry_instance->set_perception_properties(*props);
+  }
+  return geometry_instance;
 }
 
 // Explicitly instantiates on the most common scalar types.
