6399643e79ba658d1fe13baf59cf5791ed28e009
Eric Cousineau <eric.cousineau@tri.global>
2020-05-31 18:00:30 -0400

    weird output...

--- tmp/cc_docstrings_transform.py
+++ tmp/cc_docstrings_transform.py
@@ -5,14 +5,14 @@ from os.path import abspath, dirname, join, basename
 from subprocess import run, STDOUT, PIPE
 
 
-class Type(Enum):
+class Type:
     OTHER = ""
     TRIPLE_SLASH = "///"
     DOUBLE_STAR = "/**"
     SINGLE_STAR = "* "
     COMMENT_END = "*/"
 
-    PRIMARY_TYPES = (TRIPLE_SLASH, DOUBLE_START)
+    PRIMARY_TYPES = (TRIPLE_SLASH, DOUBLE_STAR)
     SECONDARY_TYPES = (OTHER, SINGLE_STAR)
 
     @staticmethod
@@ -21,12 +21,12 @@ class Type(Enum):
         end_type = Type.OTHER
         if len(text) > 0:
             # Text must already be stripped..
-            assert text[0] != " "
+            assert text[0] != " ", repr(text)
             assert text[-1] != " "
             if text.startswith("///"):
                 start_type = Type.TRIPLE_SLASH
             elif text.startswith("/**"):
-                start_type = Type.DOUBLE_STAR_ONLY
+                start_type = Type.DOUBLE_STAR
             elif text.startswith("* "):
                 start_type = Type.SINGLE_STAR
             if text.endswith("*/"):
@@ -34,8 +34,10 @@ class Type(Enum):
         return start_type, end_type
 
 
-def get_indent(line):
+def split_indent(line):
+    i = 0
     for i in range(len(line)):
+        c = line[i]
         assert c != '\t'
         if c != ' ':
             break
@@ -46,16 +48,19 @@ class Docline:
     def __init__(self, filename, num, raw_line):
         self.filename = filename
         self.num = num
-        without_indent, self.indent = split_indent(raw_line)
+        assert raw_line[-1] == "\n"
+        self.raw_line = raw_line[:-1]
+        self.indent, without_indent = split_indent(self.raw_line)
         self.start_type, self.end_type = Type.parse(without_indent)
+        # print(f"{(self.start_type, self.end_type)}: {raw_line}")
         self.text = None
         if self.start_type != Type.OTHER:
             self.text = without_indent[len(self.start_type):]
             if self.end_type == Type.COMMENT_END:
                 self.text = self.text[:-len("*/")].rstrip("*")
 
-    def __repr__(self):
-        return f"<Docline {basename(self.filename)}:{self.num}>"
+    def __str__(self):
+        return f"{self.filename}:{self.num}: {self.raw_line}"
 
 
 class Docstring:
@@ -64,19 +69,17 @@ class Docstring:
         self.lines = [line]
         self.primary_type = line.start_type
         self.secondary_type = Type.OTHER
-        self.done = False
+        self._done = False
 
     def maybe_add_next_line(self, line):
         add_line = False
-        require_done_if_not_added = False
-        if self.lines[0].type == Type.TRIPLE_SLASH:
+        if self.primary_type == Type.TRIPLE_SLASH:
             if line.start_type == Type.TRIPLE_SLASH:
                 add_line = True
             else:
-                self.done = True
-        elif self.lines[0].type == Type.DOUBLE_STAR:
-            require_done_if_not_added = True
-            if line.start_type in TYPE.SECONDARY_TYPES:
+                self._done = True
+        elif self.primary_type == Type.DOUBLE_STAR:
+            if line.start_type in Type.SECONDARY_TYPES:
                 if len(self.lines) == 1:
                     self.secondary_type = line.start_type
                     add_line = True
@@ -84,42 +87,58 @@ class Docstring:
                         add_line = True
             if line.end_type == Type.COMMENT_END:
                 add_line = True
-                self.done = True
+                self._done = True
+        else:
+            assert False
         if add_line:
             self.lines.append(line)
             return True
         else:
-            if require_done_if_not_added:
-                assert self.done, line
             return False
 
+    def is_finished(self):
+        return self._done
 
-def parse_docstrings(f):
-    raw_lines = list(f.readlines())
-    filename = abspath(f.name)
+    def finish(self):
+        if self.primary_type == Type.DOUBLE_STAR:
+            # print(self)
+            assert self._done, f"Needs termination: {self.lines[-1]}"
+        else:
+            self._done = True
+
+    def __str__(self):
+        # text_lines = ["---"] + [str(x) for x in self.lines]
+        # print(text_lines)
+        return "" #"\n".join(text_lines)
+
+
+def parse_docstrings(filename, raw_lines):
     docstrings = []
     docstring = None
     for num, raw_line in enumerate(raw_lines):
-        line = Docline(num, raw_line)
-        if docstring is None:
+        line = Docline(filename, num, raw_line)
+        if docstring is None or docstring.is_finished():
             if line.start_type in Type.PRIMARY_TYPES:
                 docstring = Docstring(line)
         else:
-            docstring.maybe_add_next_line(line)
-            if docstring.done():
+            if not docstring.maybe_add_next_line(line):
+                docstring.finish()
                 docstrings.append(docstring)
-                docstring = None
-    if docstring is not None:
-        end_line = Docline(-1, "")
-        docstring.maybe_add_next_line(end_line)
-        # File has docstring syntax error?
-        assert docstring.done(), filename
+    if docstring is not None and not docstring.is_finished():
+        docstring.finish()
         docstrings.append(docstring)
-    return docstrings
+    return docstrings, raw_lines
 
 
 def main():
-    filename = 
+    filename = "common/cond.h"
+
+    with open(filename, "r") as f:
+        raw_lines = list(f.readlines())
+        docstrings = parse_docstrings(f.name, raw_lines)
+    for docstring in docstrings:
+        print(repr(docstring))
+        print("---")
 
 
 assert  __name__ == "__main__"
