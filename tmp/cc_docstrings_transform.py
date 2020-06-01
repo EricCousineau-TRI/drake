aaa251901a1c0ef9ae89701786946e9b43e14fb4
Eric Cousineau <eric.cousineau@tri.global>
2020-05-31 23:41:41 -0400

    fixup

--- tmp/cc_docstrings_transform.py
+++ tmp/cc_docstrings_transform.py
@@ -266,6 +266,9 @@ def format_docstring(docstring):
             return [f"{new_line}{suffix}"]
 
     if len(text_lines) == 1:
+        if "://" in first_line:
+            # Weird behavior with bogus lint?
+            return [f"{indent}/// {first_line}"]
         new_lines = maybe_wrap(f"/** {first_line}", " */")
     else:
         new_lines = [f"{indent}/** {first_line}"]
