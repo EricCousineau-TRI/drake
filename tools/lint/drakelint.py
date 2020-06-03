f8ddfe3613e1cb1da222e468446b95faad877504
Eric Cousineau <eric.cousineau@tri.global>
2020-06-03 02:58:08 -0400

    yawr

--- tools/lint/drakelint.py
+++ tools/lint/drakelint.py
@@ -51,9 +51,11 @@ def _check_includes(filename):
 
 
 def _check_cpp_docstrings(filename):
+    return 0
     if cpp_docstring_lint.is_ignored_file(filename):
         return 0
-    errors = cpp_docstring_lint.check_or_apply_lint_on_file(filename, check_lint=True)
+    errors = cpp_docstring_lint.check_or_apply_lint_on_file(
+        filename, check_lint=True)
     if errors:
         print()
         # Chokes on solvers/minimum_value_constraint.h ?
