7caffc1df5a179176a1c8a59b71070e95fdf7235
Eric Cousineau <eric.cousineau@tri.global>
2020-07-29 15:55:38 -0400

    update script

--- tmp/convert.py
+++ tmp/convert.py
@@ -28,17 +28,31 @@ def main():
         if file.startswith("./attic/"):
             files.remove(file)
     
-    for file in files:
+    for file in []: #files:
+        print(file)
         result = ign_sdf(
             "check",
             basename(file),
             cwd=dirname(file),
             stderr=STDOUT,
         )
-        print(file)
         print(result.returncode)
         print(result.stdout)
         print("---")
+
+    for file in files:
+        print(file)
+        result = ign_sdf(
+            "print",
+            file,
+            stderr=PIPE,
+        )
+        if result.returncode == 0:
+            with open(file, "w") as f:
+                f.write(result.stdout)
+        else:
+            print(result.returncode)
+            print(result.stderr)
 
 
 assert __name__ == "__main__"
