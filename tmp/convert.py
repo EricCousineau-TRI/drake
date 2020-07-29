5f5ae32b664347b1abdf619fbe405bf98534b291
Eric Cousineau <eric.cousineau@tri.global>
2020-07-29 16:04:52 -0400

    setup perl pie

--- tmp/convert.py
+++ tmp/convert.py
@@ -2,7 +2,9 @@
 
 import os
 from os.path import basename, dirname, abspath
+import re
 from subprocess import run, PIPE, STDOUT
+
 
 def ign_sdf(cmd, sdf_file, **kwargs):
     ign_sdf = abspath("bazel-bin/tools/workspace/sdformat/ign_sdf")
@@ -12,6 +14,79 @@ def ign_sdf(cmd, sdf_file, **kwargs):
         encoding="utf8",
         **kwargs
     )
+
+
+def check(file):
+    result = ign_sdf(
+        "check",
+        basename(file),
+        cwd=dirname(file),
+        stderr=STDOUT,
+    )
+    print(result.returncode)
+    print(result.stdout)
+
+
+def convert(file):
+    result = ign_sdf(
+        "print",
+        file,
+        stderr=PIPE,
+    )
+    if result.returncode == 0:
+        with open(file, "w") as f:
+            f.write(result.stdout)
+    else:
+        print(result.returncode)
+        print(result.stderr)
+
+
+class Replace:
+    def __init__(self, from_, to_):
+        self.from_ = re.compile(from_)
+        self.to_ = to_
+
+    def __call__(self, text):
+        return re.sub(self.from_, self.to_, text)
+
+
+reps = [
+    Replace(
+        from_=r"<sdf (.*?)version=('.*?'|\".*?\")",
+        to_=r"<sdf \1version=\"1.7\""
+    ),
+    Replace(
+        from_=r"pose frame=(''|\"\")",
+        to_="pose",
+    ),
+    Replace(
+        from_=r"<\?xml.*?\?>\n",
+        to_="",
+    ),
+    Replace(
+        from_="'",
+        to_="\"",
+    ),
+    Replace(
+        from_=" -0 ",
+        to_=" 0 ",
+    ),
+    Replace(
+        from_=r"<pose(.*?)>\s*0\s+0\s+0\s+0\s+0\s+0</pose>",
+        to_=r"<pose\1/>",
+    )
+]
+
+
+def perl_pie(file):
+    with open(file, encoding="utf8") as f:
+        text = f.read().strip()
+    for rep in reps:
+        text = rep(text)
+    with open(file, "w", encoding="utf8") as f:
+        f.write('<?xml version="1.0"?>\n')
+        f.write(text.strip() + "\n")
+
 
 # First, check all models.
 def main():
@@ -27,33 +102,15 @@ def main():
     for file in list(files):
         if file.startswith("./attic/"):
             files.remove(file)
-    
-    for file in []: #files:
-        print(file)
-        result = ign_sdf(
-            "check",
-            basename(file),
-            cwd=dirname(file),
-            stderr=STDOUT,
-        )
-        print(result.returncode)
-        print(result.stdout)
-        print("---")
 
     for file in files:
         print(file)
-        result = ign_sdf(
-            "print",
-            file,
-            stderr=PIPE,
-        )
-        if result.returncode == 0:
-            with open(file, "w") as f:
-                f.write(result.stdout)
-        else:
-            print(result.returncode)
-            print(result.stderr)
+        # check(file)
+        # convert(file)
+        perl_pie(file)
+        print("---")
 
 
 assert __name__ == "__main__"
 main()
+
