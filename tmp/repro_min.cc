d8501d6ec06821b42c63efe69217ef6234389b0f
Eric Cousineau <eric.cousineau@tri.global>
2020-09-03 17:08:22 -0400

    add primer

--- tmp/repro_min.cc
+++ tmp/repro_min.cc
@@ -1,6 +1,7 @@
 #include <gflags/gflags.h>
 
 #include "drake/common/text_logging.h"
+#include "drake/common/unused.h"
 #include "drake/geometry/render/camera_properties.h"
 #include "drake/geometry/render/render_engine_vtk_factory.h"
 
@@ -25,6 +26,14 @@ void EmptyRender(int render_count) {
 }
 
 int main(int argc, char* argv[]) {
+  auto renderer = MakeRenderEngineVtk(RenderEngineVtkParams());
+  CameraProperties camera_prop(
+      640, 480, M_PI / 4, "doesn't matter");
+  ImageRgba8U image(camera_prop.width, camera_prop.height);
+  drake::log()->info("Priming...");
+  renderer->RenderColorImage(camera_prop, false, &image);
+
+  unused(renderer);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   for (int i = 0; i < FLAGS_count; ++i) {
     drake::log()->info("i: {}", i);
