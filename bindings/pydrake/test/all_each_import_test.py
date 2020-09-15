67b10053cc211b1ff462f92bf4fa24bf2a351664
Eric Cousineau <eric.cousineau@tri.global>
2020-09-15 16:50:16 -0400

    pydrake: Add all_each_import_test

---
+++ bindings/pydrake/test/all_each_import_test.py
@@ -0,0 +1,27 @@
+import multiprocessing as mp
+import subprocess
+import sys
+import unittest
+
+import pydrake.all
+
+
+def check_module(name):
+    # Run a new interpreter and ensure we can import the module in isolation.
+    print(name)
+    subprocess.run([sys.executable, "-c", f"import {name}"], check=True)
+
+
+class Test(unittest.TestCase):
+    def test_each_import(self):
+        names = []
+        for name in sys.modules.keys():
+            if not name.startswith("pydrake.") or "._" in name:
+                continue
+            if name == "pydrake.all":
+                continue
+            names.append(name)
+        self.assertGreater(len(names), 0)
+
+        with mp.Pool(4) as pool:
+            pool.map(check_module, names)
