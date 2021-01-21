fc97f6fca924a879e98414b0432170b1210697b7
Eric Cousineau <eric.cousineau@tri.global>
2021-01-21 17:47:50 -0500

    show "un-deprecated" state

--- bindings/pydrake/common/test/deprecation_example/example_class.h
+++ bindings/pydrake/common/test/deprecation_example/example_class.h
@@ -12,36 +12,27 @@ class ExampleCppClass {
   /// Good constructor.
   ExampleCppClass() {}
 
-  // N.B. This spacer is to ensure that the class's documentation does not pick
-  // up the following deprecation below.
-
-  DRAKE_DEPRECATED("2038-01-19", "Do not use ExampleCppClass(int).")
+  /// ctor(int) will be deprecated.
   explicit ExampleCppClass(int x) { unused(x); }
 
-  DRAKE_DEPRECATED("2038-01-19", "Do not use ExampleCppClass(double).")
+  /// ctor(double) will be deprecated.
   explicit ExampleCppClass(double y) { unused(y); }
 
-  DRAKE_DEPRECATED("2038-01-19", "Do not use DeprecatedMethod().")
+  /// This will be full deprecated.
   void DeprecatedMethod() {}
-
-  /// Good property.
-  int prop{};
-
-  // N.B. It doesn't really make sense to show a DRAKE_DEPRECATED property here
-  // because that causes the whole class to be deprecated.
 
   /// Good overload.
   void overload() {}
 
-  DRAKE_DEPRECATED("2038-01-19", "Do not use overload(int).")
+  /// This will be deprecated.
   void overload(int x) { unused(x); }
+
+  /// Not yet deprecated.
+  int deprecated_aliased_prop{};
 };
 
-/// Serves as an example for binding (and deprecating) a simple struct. This
-/// allows the struct to be constructed with ParamInit and deprecated using
-/// the corresponding DeprecatedParamInit.
-struct DRAKE_DEPRECATED(
-    "2038-01-19", "Do not use ExampleCppStruct") ExampleCppStruct {
+/// Simple struct.
+struct ExampleCppStruct {
   int i{};
   int j{};
 };
--- bindings/pydrake/common/test/deprecation_example_test.py
+++ bindings/pydrake/common/test/deprecation_example_test.py
@@ -11,50 +11,27 @@ For more details, please review the following documentation:
 
 import unittest
 
-from pydrake.common.test_utilities.deprecation import catch_drake_warnings
-
 import deprecation_example as mut
 import deprecation_example.cc_module as mut_cc
 
 
 class TestDeprecationExample(unittest.TestCase):
     def test_example_cpp_struct(self):
-        with catch_drake_warnings(expected_count=1) as w:
-            obj = mut_cc.ExampleCppStruct()
-        self.assertIn("Do not use ExampleCppStruct", str(w[0].message))
-
+        obj = mut_cc.ExampleCppStruct()
         self.assertEqual(obj.i, 0)
         self.assertEqual(obj.j, 0)
 
     def test_example_cpp_class_ctors(self):
         mut_cc.ExampleCppClass()
-
-        with catch_drake_warnings(expected_count=1) as w:
-            mut_cc.ExampleCppClass(0)
-        self.assertIn("Do not use ExampleCppClass(int)", str(w[0].message))
-        with catch_drake_warnings(expected_count=1) as w:
-            mut_cc.ExampleCppClass(0.0)
-        self.assertIn("Do not use ExampleCppClass(double)", str(w[0].message))
+        mut_cc.ExampleCppClass(0)
+        mut_cc.ExampleCppClass(0.0)
 
     def test_example_cpp_class_methods(self):
         obj = mut_cc.ExampleCppClass()
-
-        with catch_drake_warnings(expected_count=1) as w:
-            obj.DeprecatedMethod()
-        self.assertIn("Do not use DeprecatedMethod()", str(w[0].message))
-
-        # Not deprecated.
+        obj.DeprecatedMethod()
         obj.overload()
-
-        with catch_drake_warnings(expected_count=1) as w:
-            obj.overload(0)
-        self.assertIn("Do not use overload(int)", str(w[0].message))
+        obj.overload(0)
 
     def test_example_cpp_class_properties(self):
         obj = mut_cc.ExampleCppClass()
-
-        self.assertEqual(obj.prop, 0)
-
-        with catch_drake_warnings(expected_count=1) as w:
-            self.assertEqual(obj.deprecated_aliased_prop, 0)
-        self.assertIn("Do not use deprecated_aliased_prop", str(w[0].message))
+        self.assertEqual(obj.deprecated_aliased_prop, 0)
