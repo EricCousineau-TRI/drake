e8b3dceeeccef1584f52ad65edb7f899f4bfe1c6
Eric Cousineau <eric.cousineau@tri.global>
2021-03-29 17:05:00 -0400

    default_scalars_pybind: Consolidate CopyIfNotPodType functions
    Add doxygen group
    Switch docstrings from /// to use /** */
    Fix minor typos in helper functions

--- bindings/pydrake/common/default_scalars_pybind.h
+++ bindings/pydrake/common/default_scalars_pybind.h
@@ -15,7 +15,7 @@ namespace pydrake {
 
 // N.B. This should be kept in sync with the `*_DEFAULT_SCALARS` macro in
 // `default_scalars.h`.
-/// Type pack defining common scalar types.
+/** Type pack defining common scalar types. */
 using CommonScalarPack = type_pack<  // BR
     double,                          //
     AutoDiffXd,                      //
@@ -23,14 +23,31 @@ using CommonScalarPack = type_pack<  // BR
 
 // N.B. This should be kept in sync with the `*_DEFAULT_NONSYMBOLIC_SCALARS`
 // macro in `default_scalars.h`.
-/// Type pack for non-symbolic common scalar types.
+/** Type pack for non-symbolic common scalar types. */
 using NonSymbolicScalarPack = type_pack<  // BR
     double,                               //
     AutoDiffXd>;
 
-// TODO(eric.cousineau): Simplify this (#8116).
-/// Permits referencing for builtin dtypes (e.g. T = double), but then switches
-/// to copying for custom dtypes (T = {AutoDiffXd, Expression}).
+// TODO(eric.cousineau): Remove `CopyIfNotPodType` functions and
+// `return_value_policy_for_scalar_type` once #8116 is resolved.
+
+/** @name Handling special non-POD scalar types.
+
+Because we use dtype=object in NumPy, we cannot alias (pass references) the
+data underlying matrix objects when sharing data between NumPy and Eigen
+(#8116).
+
+The simple policy these functions help enforce:
+
+- When T is double, allow referencing (do not require copying).
+- When T is not double (i.e., AutoDiffXd, Expression), copy data (do not
+  reference the data).
+ */
+//@{
+
+/** Permits referencing for builtin dtypes (e.g. T == double), but then switches
+ to copying for custom dtypes (T ∈ {AutoDiffXd, Expression}).
+*/
 template <typename T>
 py::return_value_policy return_value_policy_for_scalar_type() {
   if (std::is_same<T, double>::value) {
@@ -39,6 +56,35 @@ py::return_value_policy return_value_policy_for_scalar_type() {
     return py::return_value_policy::copy;
   }
 }
+
+/** References VectorX<T == double>'s data (does not copy). **/
+inline Eigen::VectorBlock<const VectorX<double>> CopyIfNotPodType(
+    Eigen::VectorBlock<const VectorX<double>> x) {
+  return x;
+}
+
+/** Copies VectorX<T != double> data (does not copy). **/
+template <typename T>
+VectorX<T> CopyIfNotPodType(Eigen::VectorBlock<const VectorX<T>> x) {
+  return x;
+}
+
+/** References MatrixX<T == double> data (does not copy). **/
+inline Eigen::Block<const MatrixX<double>, Eigen::Dynamic, Eigen::Dynamic, true>
+CopyIfNotPodType(
+    Eigen::Block<const MatrixX<double>, Eigen::Dynamic, Eigen::Dynamic, true>
+        x) {
+  return x;
+}
+
+/** Copies MatrixX<T != double>  data (does not copy). **/
+template <typename T>
+MatrixX<T> CopyIfNotPodType(
+    Eigen::Block<const MatrixX<T>, Eigen::Dynamic, Eigen::Dynamic, true> x) {
+  return x;
+}
+
+//@}
 
 namespace internal {
 // For generic types, only permit conversion to the type itself.
@@ -55,7 +101,7 @@ struct CastUPack<double> {
 };
 }  // namespace internal
 
-/// Binds `cast<T>()` explicitly.
+/** Binds `cast<T>()` explicitly. */
 template <typename T, typename PyClass,
     typename UPack = typename internal::CastUPack<T>::Pack>
 void DefCast(PyClass* cls, const char* doc, UPack U_pack = {}) {
