a80639f62ce38356727070c6b064085ff9eac6ea
Eric Cousineau <eric.cousineau@tri.global>
2021-07-28 17:58:17 -0400

    more compact

--- tmp/tmp_py.cc
+++ tmp/tmp_py.cc
@@ -1,46 +1,25 @@
 #include "drake/bindings/pydrake/common/deprecation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
-#include "drake/systems/framework/leaf_system.h"
+#include "drake/common/drake_copyable.h"
 
 namespace drake {
 namespace pydrake {
 
-class FooSystem : public systems::LeafSystem<double> {
- public:
-  FooSystem() : systems::LeafSystem<double>() {
-    port1_ =
-        this->DeclareAbstractOutputPort("port1", &FooSystem::Calc1).get_index();
-    port2_ =
-        this->DeclareAbstractOutputPort("port2", &FooSystem::Calc1).get_index();
-  }
-  const systems::OutputPort<double>& get_output_port1() const {
-    return systems::System<double>::get_output_port(port1_);
-  }
-  const systems::OutputPort<double>& get_output_port2() const {
-    return systems::System<double>::get_output_port(port2_);
-  }
+struct NonCopyable {
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(NonCopyable)
+  NonCopyable() {}
+};
 
+class Container {
+ public:
+  const NonCopyable& get_noncopyable() const { return noncopyable_; }
  private:
-  void Calc1(const systems::Context<double>& context, double* output) const {
-    *output = context.get_time() + 1.0;
-  }
-  void Calc2(const systems::Context<double>& context, double* output) const {
-    *output = context.get_time() + 2.0;
-  }
-  int port1_{-1};
-  int port2_{-1};
+  NonCopyable noncopyable_;
 };
 
 void BindStuff(py::module m) {
-  using Class = FooSystem;
-  py::class_<Class> cls(m, "FooSystem", "class doc");
-  cls  // BR
-      .def(py::init(), "Constructor")
-      .def("get_output_port1", &Class::get_output_port1,
-          py_rvp::reference_internal, "Port 1 doc")
-      .def("get_output_port2", 
-          WrapDeprecated("Deprecated 2038-01-01", &Class::get_output_port2),
-          py_rvp::reference_internal, "Port 2 doc");
+  auto wrapped = WrapDeprecated("Deprecated", &Container::get_noncopyable);
+  py::class_<Container>(m, "Container").def("get_noncopyable", wrapped);
 }
 
 PYBIND11_MODULE(tmp, m) {
