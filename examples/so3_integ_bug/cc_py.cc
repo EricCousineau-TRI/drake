e31ba0cb2c43fa559ff574c37e9a9bd4be1cbde6
Eric Cousineau <eric.cousineau@tri.global>
2023-02-19 22:48:00 -0500

    k, need to debug this stuff

---
+++ examples/so3_integ_bug/cc_py.cc
@@ -0,0 +1,28 @@
+#include "pybind11/eigen.h"
+#include "pybind11/pybind11.h"
+
+#include "drake/multibody/tree/quaternion_rate.h"
+
+namespace drake {
+namespace pydrake {
+
+PYBIND11_MODULE(cc, m) {
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::multibody;
+
+  py::module::import("pydrake.common.eigen_geometry");
+
+  {
+    using Class = QuaternionRate;
+    py::class_<Class>(m, "QuaternionRate")
+        .def_static(
+            "AngularVelocityToQuaternionRateMatrix",
+            &Class::AngularVelocityToQuaternionRateMatrix)
+        .def_static(
+            "QuaternionRateToAngularVelocityMatrix",
+            &Class::QuaternionRateToAngularVelocityMatrix);
+  }
+}
+
+}  // namespace pydrake
+}  // namespace drake
